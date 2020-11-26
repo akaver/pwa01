6b9f1371786723daf41615c24a96df0e8b613e73
Andres Käver <akaver@akaver.com>
2020-11-27 00:32:01 +0200

    stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/standard',
+        '@vue/typescript/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'indent': ["warn", 4],
+        'space-before-function-paren': 'off',
+        'quotes': 'off',
+        "comma-dangle": 'off',
+        'semi': ['warn', 'always'],
+    }
 }
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    pwa: {
+        /*
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'service-worker.js',
+            // ...other Workbox options...
+        },
+        */
+        name: 'VUE-PWA',
+        themeColor: '#42b983',
+        msTileColor: '#42b983',
+        appleMobileWebAppCache: 'yes',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+
+        manifestOptions: {
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            background_color: '#42b983'
+        }
+    }
+};
