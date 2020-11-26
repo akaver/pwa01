f376c129683a34de2a1c1cc178d1b7f242035dd3
Andres Käver <akaver@akaver.com>
2020-11-27 00:39:20 +0200

    docs

---
+++ docs/dist/precache-manifest.10465a62f3a92c89fdbc9a7dd8ca4a37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e153aab3f034e10827",
+    "url": "/pwa01/css/app.e0291dd3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa01/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "37114f73390343a4c1ed416cf8019f59",
+    "url": "/pwa01/index.html"
+  },
+  {
+    "revision": "8bbbba172f4fdd27ac39",
+    "url": "/pwa01/js/about.61e710b8.js"
+  },
+  {
+    "revision": "24e153aab3f034e10827",
+    "url": "/pwa01/js/app.f7445d6b.js"
+  },
+  {
+    "revision": "66f6885ad7a1a544877e",
+    "url": "/pwa01/js/chunk-vendors.a857519c.js"
+  },
+  {
+    "revision": "d18abc961a69621df87c49b30816518e",
+    "url": "/pwa01/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa01/robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/pwa01/'
+        : '/',
     pwa: {
         /*
         workboxPluginMode: 'InjectManifest',
